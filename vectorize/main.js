ac933b59cf84c3c9b9bf3ed1f3c966746f1f4b5f
inikonzs <zhanzs@mail.ru>
2024-09-06 11:50:22 +0200

    changed vectorize demo

---
+++ vectorize/main.js
@@ -0,0 +1,4 @@
+import { initUI } from "./ui.js";
+import { toSvgPath } from "./toSvgPath.js";
+
+initUI();
--- vectorize/ui.js
+++ vectorize/ui.js
@@ -7,6 +7,7 @@ import { checkFigure } from './fillFigure.js';
 const svg = document.querySelector('.dest-svg');
 const canvas = document.querySelector('.source-canvas');
 const input = document.querySelector('.file-input');
+const threshold = document.querySelector('.threshold-input');
 
 function toBitmap(ctx, pos) {
     const data = ctx.getImageData(0, 0, canvas.width, canvas.height);
@@ -24,7 +25,7 @@ function toBitmap(ctx, pos) {
             }
             const maxDiff = Math.max(...colors.map((cl, cli)=> Math.abs(cl  - selectColor[cli])))
             //row.push(data.data[i * data.width * 4 + j * 4] == 0 ? '1' : '0');
-            row.push(maxDiff < 40 ? '1' : '0');
+            row.push(maxDiff < threshold.value ? '1' : '0');
         }
         arr.push(row);
     }
@@ -33,22 +34,39 @@ function toBitmap(ctx, pos) {
 
 export function initUI() {
     const ctx = canvas.getContext('2d');
+    let img;
     input.onchange = () => {
         processFile(input.files[0], (image) => {
             canvas.width = image.naturalWidth;
             canvas.height = image.naturalHeight;
+            img = image;
             ctx.drawImage(image, 0, 0);
         });
     };
+    threshold.onchange = ()=>{
+        canvas.onclick();
+    }
+    let clickPoint;
     canvas.onclick = (e) => {
-        const clickPoint = { x: e.offsetX, y: e.offsetY }; 
+        console.log('1234');
+        const bounds = canvas.getBoundingClientRect();
+        const scale = {x: bounds.width / canvas.width, y: bounds.height / canvas.height};
+        const clickPos = {x: Math.floor(e.offsetX / scale.x), y: Math.floor(e.offsetY / scale.y)};
+        console.log(clickPos);
+        ctx.drawImage(img, 0, 0);
+            //const clickPoint = { x: e.offsetX, y: e.offsetY }; 
+        //}
+        clickPoint = e ?{ x: clickPos.x, y: clickPos.y } : clickPoint;
+        if (!clickPoint){
+            return;
+        }
         //ctx.fillRect(clickPoint.x, clickPoint.y, 5, 5);
         const [bitmap, color] = toBitmap(ctx, clickPoint);
         const figPoints = checkFigure(bitmap.map((row, y)=>row.map((cell, x)=>({value: cell, isLocked: cell == '0', x, y}))), clickPoint);
         const clearBitmap = new Array(bitmap.length).fill(null).map(it=>new Array(bitmap[0].length).fill('0'));
         figPoints.forEach(it=> {
             clearBitmap[it.y][it.x] = '1';
-            ctx.fillRect(it.x, it.y, 1,1);
+            //ctx.fillRect(it.x, it.y, 1,1);
         });
         const clickVal = clearBitmap[clickPoint.y][clickPoint.x];
         let point; //= {...clickPoint};

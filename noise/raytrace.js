fddbaefcd2f3d81f106a0fa47c5c6a6ddc9aa055
InikonZS <zhanzs@mail.ru>
2025-07-15 19:31:08 +0200

    editable raytrace

--- noise/raytrace.js
+++ noise/raytrace.js
@@ -49,33 +49,73 @@ const app = ()=>{
     const size = 20;
     ctx.fillStyle = '#ccc';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    const traced = raytrace(chunk);
+    let cursorPos = null;
+    let traced = raytrace(chunk);
+    canvas.onmousemove = (e)=>{
+        //console.log(e.offsetX)
+        cursorPos = {
+            x: Math.floor(e.offsetX / size), 
+            y: Math.floor(e.offsetY / size)
+        };
 
-    chunk.forEach((it, i)=>
-        it.forEach((jt, j)=>{
-            if (jt == '8'){
-                ctx.strokeStyle = '#f002';
-            } else {
-                 ctx.strokeStyle = '#0002';
-            }
-            ctx.strokeRect(j* size, i* size, size, size);
-        })
-    );
-    ctx.lineWidth = 1;
-    traced.forEach(it=>{
-        if (it.level == -1){
-            ctx.strokeStyle = '#090';
-            ctx.fillStyle = '#050'
-        } else {
-            ctx.strokeStyle = '#000';
-            ctx.fillStyle = grey(it.level * 16);
-            ctx.fillRect(it.x* size, it.y* size, size, size);
+        if (!(cursorPos.x>=0 && cursorPos.x < chunk[0].length && cursorPos.y>=0 && cursorPos.y < chunk.length)){
+            cursorPos = null;
         }
-        ctx.fillRect(it.x* size, it.y* size, size, size);
-        ctx.strokeRect(it.x* size, it.y* size, size, size);
-        ctx.strokeStyle = '#099';
-        ctx.strokeText(it.level, it.x* size +4, (it.y +1)* size -4);
-    });
+        draw();
+    }
+
+    canvas.onclick = ()=>{
+        if (cursorPos){
+            console.log(chunk[cursorPos.y][cursorPos.x]);
+            if (chunk[cursorPos.y][cursorPos.x] == '8'){
+                chunk[cursorPos.y][cursorPos.x] = '-'
+            } else if (chunk[cursorPos.y][cursorPos.x] == '-'){
+                chunk[cursorPos.y][cursorPos.x] = 't'
+            } else if (chunk[cursorPos.y][cursorPos.x] == 't'){
+                chunk[cursorPos.y][cursorPos.x] = '8'
+            };
+            traced = raytrace(chunk);
+        }
+        draw();
+    }
+
+
+    const draw = ()=>{
+        chunk.forEach((it, i)=>
+            it.forEach((jt, j)=>{
+                if (jt == '8'){
+                    ctx.strokeStyle = '#f002';
+                } else {
+                    ctx.strokeStyle = '#0002';
+                }
+                ctx.strokeRect(j* size, i* size, size, size);
+            })
+        );
+        ctx.lineWidth = 1;
+        traced.forEach(it=>{
+            if (it.level == -1){
+                ctx.strokeStyle = '#090';
+                ctx.fillStyle = '#050'
+            } else if (it.level == 15){
+                ctx.strokeStyle = '#000';
+                ctx.fillStyle = '#ff0';
+                ctx.fillRect(it.x* size, it.y* size, size, size);
+            } else {
+                ctx.strokeStyle = '#000';
+                ctx.fillStyle = grey(it.level * 16);
+                ctx.fillRect(it.x* size, it.y* size, size, size);
+            }
+            ctx.fillRect(it.x* size, it.y* size, size, size);
+            ctx.strokeRect(it.x* size, it.y* size, size, size);
+            ctx.strokeStyle = '#099';
+            ctx.strokeText(it.level, it.x* size +4, (it.y +1)* size -4);
+        });
+        ctx.fillStyle = '#f007';
+        if (cursorPos){
+            ctx.fillRect(cursorPos.x* size,cursorPos.y* size, size, size);
+        }
+    }
+    draw();
 }
 
 const raytrace = (_chunk)=>{
